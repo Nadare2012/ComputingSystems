adb1e5dc94c545c5221818f836a5969763cd10c9
nadare2012 <nadare2012@gmail.com>
2020-03-29 19:03:36 +0500

    Finished LR1_2

--- LR1_2/LR1_2.c
+++ LR1_2/LR1_2.c
@@ -35,6 +35,60 @@ DWORD WINAPI my_thread_func2(LPVOID lp_parameter)
 	ExitThread(0);
 }
 
+DWORD WINAPI my_thread_func3(LPVOID lp_parameter)
+{
+	BOOL* active = (BOOL*)lp_parameter;
+	*active = TRUE;
+	computing_func(INT32_MAX / 2);
+	*active = FALSE;
+	ExitThread(0);
+}
+
+DWORD WINAPI my_thread_func4(LPVOID lp_parameter)
+{
+	BOOL* active = (BOOL*)lp_parameter;
+	*active = TRUE;
+	computing_func(INT32_MAX / 2);
+	*active = FALSE;
+	ExitThread(0);
+}
+
+DWORD WINAPI my_thread_func5(LPVOID lp_parameter)
+{
+	BOOL* active = (BOOL*)lp_parameter;
+	*active = TRUE;
+	computing_func(INT32_MAX / 2);
+	*active = FALSE;
+	ExitThread(0);
+}
+
+DWORD WINAPI my_thread_func6(LPVOID lp_parameter)
+{
+	BOOL* active = (BOOL*)lp_parameter;
+	*active = TRUE;
+	computing_func(INT32_MAX / 2);
+	*active = FALSE;
+	ExitThread(0);
+}
+
+DWORD WINAPI my_thread_func7(LPVOID lp_parameter)
+{
+	BOOL* active = (BOOL*)lp_parameter;
+	*active = TRUE;
+	computing_func(INT32_MAX / 2);
+	*active = FALSE;
+	ExitThread(0);
+}
+
+DWORD WINAPI my_thread_func8(LPVOID lp_parameter)
+{
+	BOOL* active = (BOOL*)lp_parameter;
+	*active = TRUE;
+	computing_func(INT32_MAX / 2);
+	*active = FALSE;
+	ExitThread(0);
+}
+
 void init_thread(void)
 {
 	SYSTEM_INFO system_info;
@@ -44,13 +98,31 @@ void init_thread(void)
 
 	DWORD thread_id1;
 	DWORD thread_id2;
+	DWORD thread_id3;
+	DWORD thread_id4;
+	DWORD thread_id5;
+	DWORD thread_id6;
+	DWORD thread_id7;
+	DWORD thread_id8;
 	BOOL active1 = TRUE;
 	BOOL active2 = TRUE;
+	BOOL active3 = TRUE;
+	BOOL active4 = TRUE;
+	BOOL active5 = TRUE;
+	BOOL active6 = TRUE;
+	BOOL active7 = TRUE;
+	BOOL active8 = TRUE;
 
 	HANDLE h_thread1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func1, (void*)&active1, 0, &thread_id1);
 	HANDLE h_thread2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func2, (void*)&active2, 0, &thread_id2);
+	HANDLE h_thread3 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func3, (void*)&active3, 0, &thread_id3);
+	HANDLE h_thread4 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func4, (void*)&active4, 0, &thread_id4);
+	HANDLE h_thread5 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func5, (void*)&active5, 0, &thread_id5);
+	HANDLE h_thread6 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func6, (void*)&active6, 0, &thread_id6);
+	HANDLE h_thread7 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func7, (void*)&active7, 0, &thread_id7);
+	HANDLE h_thread8 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)my_thread_func8, (void*)&active8, 0, &thread_id8);
 
-	printf("\nThreads IDs %lu %lu \n", thread_id1, thread_id2);
+	printf("\nThreads IDs %lu %lu %lu %lu %lu %lu %lu %lu\n", thread_id1, thread_id2, thread_id3, thread_id4, thread_id5, thread_id6, thread_id7, thread_id8);
 
 	t1 = time(NULL);
 
@@ -61,10 +133,16 @@ void init_thread(void)
 
 	printf("\nProcessAffinityMask / SystemAffinityMask: %lx / %lx\n", dw_process_affinity_mask, dw_system_affinity_mask);
 
-	while (active1 == TRUE || active2 == TRUE) {}
+	while (active1 == TRUE || active2 == TRUE || active3 == TRUE || active4 == TRUE || active5 == TRUE || active6 == TRUE || active7 == TRUE || active8 == TRUE) {}
 
 	CloseHandle(h_thread1);
 	CloseHandle(h_thread2);
+	CloseHandle(h_thread3);
+	CloseHandle(h_thread4);
+	CloseHandle(h_thread5);
+	CloseHandle(h_thread6);
+	CloseHandle(h_thread7);
+	CloseHandle(h_thread8);
 }
 
 int main(int argc, char* argv[])
