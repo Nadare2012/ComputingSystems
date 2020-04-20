aae82c25de748bd8f0cc3e69e9f3666b9d0b47ae
nadare2012 <nadare2012@gmail.com>
2020-04-20 16:43:03 +0500

    Added and finished new LR2_3a, LR2_3b projects

---
+++ LR2_3a/LR2_3a.c
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <stdio.h>
+HANDLE h_sem;
+int a[5];
+HANDLE h_thread;
+unsigned long thread_id;
+
+DWORD WINAPI thread()
+{
+    int num = 0;
+    while (1)
+    {
+        for (int i = 0; i < 5; i++) a[i] = num;
+        num++;
+    }
+}
+
+int main(void)
+{
+    h_sem = CreateSemaphore(NULL, 1, 1, NULL);
+    h_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread, NULL, 0, &thread_id);
+    while (1)
+    {
+        WaitForSingleObject(h_sem, INFINITE);
+        printf("%d %d %d %d %d\n", a[0], a[1], a[2], a[3], a[4]);
+        ReleaseSemaphore(h_sem, 1, NULL);
+    }
+}
